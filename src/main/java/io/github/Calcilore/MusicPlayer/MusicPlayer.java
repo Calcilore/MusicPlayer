14f65e0ef12425dd774c3c4e9de0d6e69f5eb3d0
Calcilore <skylokon@gmail.com>
2021-10-18 00:54:20 +1100

    Added Loop song, Added Loop song random, Added Get current song, Made getting song name use translations, Removed ! form getting song, Made mod translatable

--- src/main/java/io/github/Calcilore/MusicPlayer/MusicPlayer.java
+++ src/main/java/io/github/Calcilore/MusicPlayer/MusicPlayer.java
@@ -4,7 +4,9 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.resource.language.TranslationStorage;
 import net.minecraft.client.sound.AbstractSoundInstance;
 import net.minecraft.client.sound.MovingSoundInstance;
 import net.minecraft.client.sound.PositionedSoundInstance;
@@ -14,6 +16,8 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Language;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -34,38 +38,86 @@ public class MusicPlayer implements ModInitializer {
     public static final Random random = new Random();
 
     private PositionedSoundInstance sound = null;
+    private boolean isRandomLoop = false;
 
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
 
-        KeyBinding playSong = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.playsong", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "category.musicplayer.musicplayer"));
-        KeyBinding stopSong = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.stopsong", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "category.musicplayer.musicplayer"));
+        KeyBinding playSongk       = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.playsong", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_C, "category.musicplayer.musicplayer"));
+        KeyBinding stopSongk       = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.stopsong", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "category.musicplayer.musicplayer"));
+        KeyBinding playLoopk       = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.playloop", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_BRACKET, "category.musicplayer.musicplayer"));
+        KeyBinding playRandomLoopk = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.playrandomloop", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_BRACKET, "category.musicplayer.musicplayer"));
+        KeyBinding getSongk        = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.musicplayer.getsong", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_B, "category.musicplayer.musicplayer"));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (playSong.wasPressed()) {
-                SoundEvent soundEvent = songs[random.nextInt(songs.length)];
-                if (soundEvent != null) {
-                    if (sound != null)
-                        client.getSoundManager().stop(sound);
-
-                    sound = new PositionedSoundInstance(soundEvent.getId(), SoundCategory.RECORDS, 0.25f, 1f, false, 0, SoundInstance.AttenuationType.NONE, 0.0D, 0.0D, 0.0D, true);
-
-                    client.getSoundManager().play(sound);
-
-                    String soundString = String.valueOf(sound.getId());
-                    soundString = soundString.substring(soundString.lastIndexOf(".") + 1);
-                    soundString = soundString.substring(0,1).toUpperCase() + soundString.substring(1).toLowerCase();
-
-                    client.inGameHud.setOverlayMessage(Text.Serializer.fromJson("[\"\",{\"text\":\"Playing Song: \",\"color\":\"green\"},{\"text\":\"" + soundString + "\",\"bold\":true,\"color\":\"gold\"},{\"text\":\"!\",\"bold\":true,\"color\":\"green\"}]"), false);
-                }
+            if (client.player == null) { // if not in world
+                isRandomLoop = false;
             }
 
-            while (stopSong.wasPressed()) {
+            while (playSongk.wasPressed()) {
+                isRandomLoop = false;
+                playSong(getRandomSong(), false, client);
+            }
+
+            while (playLoopk.wasPressed()) {
+                isRandomLoop = true;
+                playSong(getRandomSong(), true, client);
+            }
+
+            while (playRandomLoopk.wasPressed() || (!client.getSoundManager().isPlaying(sound) && isRandomLoop)) {
+                isRandomLoop = true;
+                playSong(getRandomSong(), false, client);
+            }
+
+            while (stopSongk.wasPressed()) {
                 if (sound != null)
                     client.getSoundManager().stop(sound);
+
+                sound = null;
+                isRandomLoop = false;
+            }
+
+            while (getSongk.wasPressed()) {
+                tellSong(client);
             }
         });
+    }
+
+    public SoundEvent getRandomSong() {
+        return songs[random.nextInt(songs.length)];
+    }
+
+    public void playSong(SoundEvent soundEvent, boolean loop, MinecraftClient client) {
+        if (soundEvent != null) {
+            if (sound != null)
+                client.getSoundManager().stop(sound);
+
+            sound = new PositionedSoundInstance(soundEvent.getId(), SoundCategory.RECORDS, 0.25f, 1f, loop, 0, SoundInstance.AttenuationType.NONE, 0.0D, 0.0D, 0.0D, true);
+
+            client.getSoundManager().play(sound);
+
+            tellSong(client);
+        }
+    }
+
+    public void tellSong(MinecraftClient client) {
+
+        Language lan = TranslationStorage.getInstance();
+        String soundString;
+
+        if (sound == null) {
+            soundString = lan.get("musicplayer.nothing");
+        } else {
+
+            // convert "minecraft:music_disk.<disc>" to "item.minecraft.music_disc_<disc>.desc"
+            soundString = String.valueOf(sound.getId());
+            soundString = soundString.substring(soundString.lastIndexOf(".") + 1);
+            //soundString = soundString.substring(0,1).toUpperCase() + soundString.substring(1).toLowerCase(); // No language
+            soundString = lan.get("item.minecraft.music_disc_" + soundString + ".desc"); // With language
+        }
+
+        client.inGameHud.setOverlayMessage(Text.Serializer.fromJson("[\"\",{\"text\":\"" + lan.get("musicplayer.playingsong") + "\",\"color\":\"green\"},{\"text\":\"" + soundString + "\",\"bold\":true,\"color\":\"gold\"}]"), false);
     }
 
     public static void log(Level level, String message){
@@ -73,8 +125,8 @@ public class MusicPlayer implements ModInitializer {
     }
 
     private static final SoundEvent[] songs = new SoundEvent[] {
-        SoundEvents.MUSIC_DISC_11,
-        SoundEvents.MUSIC_DISC_13,
+        //SoundEvents.MUSIC_DISC_11,
+        //SoundEvents.MUSIC_DISC_13,
         SoundEvents.MUSIC_DISC_BLOCKS,
         SoundEvents.MUSIC_DISC_CAT,
         SoundEvents.MUSIC_DISC_CHIRP,
